f31f50f640187ddeb4a51e8b01b3e5213306392b
marquez138 <somose138@gmail.com>
2025-07-28 10:21:37 -1000

    fix-useEffect

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { assets } from '@/assets/assets'
 import OrderSummary from '@/components/OrderSummary'
 import Image from 'next/image'
