cd44e7b2710a78ed4437cf660ceccf8dff18a20f
marquez138 <somose138@gmail.com>
2025-07-31 11:53:14 -1000

    add footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,18 @@
 'use client'
-import React from "react";
-import HeaderSlider from "@/components/HeaderSlider";
-import HomeProducts from "@/components/HomeProducts";
-import Banner from "@/components/Banner";
-import NewsLetter from "@/components/NewsLetter";
-import FeaturedProduct from "@/components/FeaturedProduct";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import React from 'react'
+import HeaderSlider from '@/components/HeaderSlider'
+import HomeProducts from '@/components/HomeProducts'
+import Banner from '@/components/Banner'
+import NewsLetter from '@/components/NewsLetter'
+import FeaturedProduct from '@/components/FeaturedProduct'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const Home = () => {
   return (
     <>
-      <Navbar/>
-      <div className="px-6 md:px-16 lg:px-32">
+      <Navbar />
+      <div className='px-6 md:px-16 lg:px-32'>
         <HeaderSlider />
         <HomeProducts />
         <FeaturedProduct />
@@ -21,7 +21,7 @@ const Home = () => {
       </div>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
