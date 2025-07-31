b066df833a507a2cf4b382f6282f66f514871899
marquez138 <somose138@gmail.com>
2025-07-31 11:20:52 -1000

    product-page

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -231,7 +231,7 @@ const Product = () => {
 
   return productData ? (
     <>
-      {/* ... Navbar ... */}
+      <Navbar />
       <div className='px-6 md:px-16 lg:px-32 pt-14 space-y-10'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-16'>
           <div className='px-5 lg:px-16 xl:px-20'>
